bb227d8ce79937b03a722f0ea32b808c35831779
tommylbro <dentonthomas9@gmail.com>
2025-08-09 08:08:21 -0500

    Update 1.02.02

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './index.css';
-import App from './App';
+import 'index.css';
+import App from 'App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
 
